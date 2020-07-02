97b383a05dd15a233a165ad5b2d3a1e62aaaab53
enigmatized <54577107+enigmatized@users.noreply.github.com>
2020-07-02 12:00:21 -0700

    Adding SqlAlchemey Code

--- application/app.py
+++ application/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from app import config
+
 
 app = Flask(__name__)
+app.config.from_object (config)
+db = SQLAlchemy (app)
+
+
+
 
 
 @app.route('/')
---
+++ application/app/config.py
@@ -0,0 +1,31 @@
+import os
+
+SECRET_KEY = os.urandom (256)
+UPLOAD_FOLDER= "/home/..../application/static/images" 
+CLOUDSQL_USER = 'root'
+CLOUDSQL_PASSWORD = 'abc123'
+CLOUDSQL_DATABASE = 'testsql'
+CLOUDSQL_CONNECTION_NAME = 'my-project-250617:us-central1:testsql'
+LOCAL_SQLALCHEMY_DATABASE_URI = (
+    'mysql+pymysql://{nam}:{pas}@10.128.0.3:3306/{dbn}').format (
+    nam=CLOUDSQL_USER,
+    pas=CLOUDSQL_PASSWORD,
+    dbn=CLOUDSQL_DATABASE,
+)
+
+LIVE_SQLALCHEMY_DATABASE_URI = (
+    'mysql+pymysql://{nam}:{pas}@localhost/{dbn}?unix_socket=/cloudsql/{con}').format (
+    nam=CLOUDSQL_USER,
+    pas=CLOUDSQL_PASSWORD,
+    dbn=CLOUDSQL_DATABASE,
+    con=CLOUDSQL_CONNECTION_NAME,
+)
+
+if os.environ.get ('GAE_INSTANCE'):
+    SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
+else:
+    SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
+
+
+
+#app.config["UPLOAD_FOLDER"] =
