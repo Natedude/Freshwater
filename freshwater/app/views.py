d22ee5c3a335778c694a96116c63ffff4741ad39
Natedude <diggity123@gmail.com>
2020-07-19 17:02:44 -0700

    Directory changes: update: application -> **freshwater** add: package freshwater/**app**/ NOTE: put nearly everything is this app package update: templates/<about template file>  ->  templates/**about**/<about template file> update: images/<about image file>  ->  images/**about**/<about image file> add: package app/**search** NOTE: search.py has all the code for searching & has query route function File changes: update: app.py -> app/**views**.py NOTE: removed all non-route code add: **models**.py NOTE: where all the mysql database stuff is, all in one file update: model classes made plural by adding s to the ends update: images/<image file>  -> images/**about**/<image file> for each html with about images update: unnecesary imports were deleted or commented out delete: wsgi.py Other Notes: Instructions to run: run ```python3 freshwater/run.py```

---
+++ freshwater/app/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from app import app
+from .search import search
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template("home.html")
+
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+    return search.query()
+
+@app.route('/about')
+def about():
+    return render_template("about/about_team.html")
+
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("about/about_" + name + ".html")
