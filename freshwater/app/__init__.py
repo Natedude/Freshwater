c0fd14e4fe4afb201e0a041168069b9aabe2338d
Natedude <diggity123@gmail.com>
2020-07-19 18:11:21 -0700

    add: app/__init__ SQLALCHEMY config

--- freshwater/app/__init__.py
+++ freshwater/app/__init__.py
@@ -7,4 +7,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/CSC_2'
 # above is same line but with different port, 3307 -----------------^
 # so you can quickly change if your 3306 port is bound 
 # (for example: if you're running a local mysql server)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to suppress warning
 db = SQLAlchemy(app)
